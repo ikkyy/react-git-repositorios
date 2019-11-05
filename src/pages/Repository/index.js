7d787af9430ddc0061210a00681d72094e57ef7b
Ikkyy <msndoikky@hotmail.com>
2019-11-05 12:55:32 -0300

    Dados da API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api'
 
 // import { Container } from './styles';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true
+  }
+  async componentWillMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository)
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5
+        }
+      })
+    ])
+
+    this.setState({
+      loading: false,
+      repository: repository.data,
+      issues: issues.data
+    })
+  }
+
+
+
+  render() {
+    const { repository, issues, loading } = this.state
+    
+    return <h1>Repository:  </h1>;
+
+  }
 }
