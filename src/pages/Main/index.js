db742f27d7645bc06c83957e1dbbade0789d2c03
Ikkyy <msndoikky@hotmail.com>
2019-11-05 11:05:36 -0300

    Estilo Global

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Title } from './styles';
 
 export default function Main() {
-  return <Title error={false}>Main
-  <small>Menor</small>
+  return <Title error={false}>Olá Mundo
   </Title>;
 }
